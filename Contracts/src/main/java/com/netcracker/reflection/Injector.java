86688ff231bb8b62adf870215d0d83ae95c715fb
Savage-E <vlad.kotov1999@ya.ru>
2021-01-08 13:21:48 +0300

    add comment in Injector

--- Contracts/src/main/java/com/netcracker/reflection/Injector.java
+++ Contracts/src/main/java/com/netcracker/reflection/Injector.java
@@ -1,10 +1,12 @@
 package com.netcracker.reflection;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.List;
+import java.util.Set;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.reflections.Reflections;
 
 
@@ -15,7 +17,7 @@ import org.reflections.Reflections;
 public class Injector {
 
   private List<Object> list = new ArrayList<>();
-
+  private static final Logger logger = LogManager.getLogger(Injector.class);
 
   /**
    * Injects dependencies into a specified object.
@@ -23,10 +25,10 @@ public class Injector {
    * @param object the object to inject dependencies
    * @param <T>    the type of the class
    * @return object  with injected dependencies
-   * @throws IllegalAccessException
+   * @throws IllegalAccessException if application does not have access to the definition of the specified class, field, method or constructor.
    */
   public static <T> T inject(T object) throws IllegalAccessException {
-
+    logger.debug("Starting method inject");
     List<Object> classInject = new ArrayList<>();
     Class<T> clazz = (Class<T>) object.getClass();
     Field[] fields = clazz.getDeclaredFields();
@@ -37,6 +39,7 @@ public class Injector {
       if (f.isAnnotationPresent(AutoInjectable.class)) {
         f.setAccessible(true);
         if (f.getType().getName().contains("java.util.List")) {
+          logger.debug("Starting injection to collection");
           ParameterizedType fieldListType = (ParameterizedType) f.getGenericType();
           Class<?> fieldGenericType = (Class<?>) fieldListType.getActualTypeArguments()[0];
           getClasses(objects, fieldGenericType);
@@ -48,6 +51,7 @@ public class Injector {
             f.set(object, classInject);
           }
         } else {
+          logger.debug("Starting injection to single object");
           getClasses(objects, f.getType());
           for (Object o : objects) {
             if (o != null && f.getType().isAssignableFrom(o.getClass())) {
@@ -56,17 +60,20 @@ public class Injector {
             if (classInject.size() == 1) {
               f.set(object, classInject.get(0));
             } else {
+              logger.info("Error to inject the object");
               throw new RuntimeException("Amount of classes more than 1 (only 1 valid)");
             }
           }
         }
       }
     }
+    logger.debug("Exiting method readFrom");
     return object;
   }
 
 
   private static void getClasses(ArrayList<Object> objects, Class<?> type) {
+    logger.debug("Exiting method getClasses");
     Configuration packages = Injector.class.getAnnotation(Configuration.class);
     Reflections reflections;
     for (String pack : packages.packages()) {
@@ -74,13 +81,15 @@ public class Injector {
       Set<Class<?>> classes = reflections.getSubTypesOf((Class<Object>) type);
       for (Class<?> o : classes) {
         try {
+          logger.debug("Injecting the object");
           objects.add(o.newInstance());
         } catch (InstantiationException | IllegalAccessException e) {
-          e.printStackTrace();
+          logger.info("Error to inject the object");
+          logger.error("Exception:",e);
         }
       }
     }
-
+    logger.debug("Exiting method getClasses");
   }
 }
 
